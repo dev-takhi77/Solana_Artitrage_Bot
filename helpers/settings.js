efc799ea718a98687775c3371e734cee570f5db2
Takhi <ewantomson1103@outlook.com>
2025-05-15 02:22:35 -0700

    update: reveal of rpc

--- helpers/settings.js
+++ helpers/settings.js
@@ -1,5 +1,5 @@
 export const solanaRpcUrl =
-  "https://radial-delicate-layer.solana-mainnet.discover.quiknode.pro/124d30642a313843475e1ac3f67e59d11d55d943";
+  "";
 export const jitoAuthSecretKey =
-  "bGPsUyfqKbt6RvMA2FbVXefCoVWF2iGhsxXuK2rmTBLEJUhyj4ViHpaaY3R92VMapMbdBcUqdDDxj8SVqJVn4ZC";
+  "";
 export const poodyMint = "HswDz9xJ8pMU1XazCu5KLhiNvF7sb64HK5AJMAzWjW2p";
